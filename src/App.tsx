22150690bb104326090b9290242f9fb5c3266efb
Alexander Smith <asmit147@ycp.edu>
2020-01-09 20:05:20 -0500

    leaflet works after fixing spelling bruh

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import GoogleMap from 'google-map-react';
 import { Helmet } from "react-helmet";
 
 import MapPortal from "./view/MapPortal";
@@ -13,9 +12,6 @@ class App extends React.Component {
     return (
       <div>
         <Helmet>
-          {/* <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-            data-crossorigin=""></script>  */}
           <title>Turbo Team!</title>
         </Helmet>
         <div className="container p-3">
@@ -24,12 +20,6 @@ class App extends React.Component {
               <BreweryList/>
             </div>
             <div className="col">
-              {/* <GoogleMap
-                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY! }}
-                defaultCenter={App.defaultProps.center}
-                defaultZoom={11}
-              >
-              </GoogleMap> */}
               <MapPortal/>
             </div>
           </div>
--- src/view/MapPortal.tsx
+++ src/view/MapPortal.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import "./MapPortal.scss";
 import { Map, TileLayer } from "react-leaflet";
-// import "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-
-//  {/* <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-// integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-// data-crossorigin=""></script> */}
 
 export default class MapPortal extends React.Component {
     render() {
@@ -19,17 +14,12 @@ export default class MapPortal extends React.Component {
 
         return (
             <Map 
-                maxZoom={10}
-                attributionControl={true}
-                zoomControl={true}
-                doubleClickZoom={true}
-                scrollWheelZoom={true}
-                dragging={true}
-                animate={true}
-                easeLinearity={0.35}
+                center={point.center}
+                zoom={point.zoom}
             >
                 <TileLayer
-                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
                 />
             </Map>
         )
